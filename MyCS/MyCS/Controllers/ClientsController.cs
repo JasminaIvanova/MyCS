e5605a1078bb180ccfbbc561977388e288e9eebc
Jasmina Ivanova <jasmina.j.ivanova@gmail.com>
2021-06-14 13:06:22 +0300

    upload file

---
+++ MyCS/MyCS/Controllers/ClientsController.cs
@@ -0,0 +1,77 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCS.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCS.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Index(List<ClientData> clientsData = null)
+        {
+            clientsData = clientsData == null ? new List<ClientData>() : clientsData;
+            return View(clientsData);
+        }
+
+        [HttpPost]
+
+        public IActionResult Index(IFormFile file, [FromServices] IHostingEnvironment hostingEnvironment)
+        {
+            #region Upload CSV
+            string fileName = $"{hostingEnvironment.WebRootPath}\\files\\{file.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+            #endregion
+
+            var clientsData = this.GetClientsDataList(file.FileName);
+            return Index(clientsData);
+        }
+
+        private List<ClientData> GetClientsDataList(string fileName)
+        {
+            List<ClientData> clientsData = new List<ClientData>();
+            #region Read CSV
+            var path = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\files"}" + "\\" + fileName;
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var clientData = csv.GetRecord<ClientData>();
+                    clientsData.Add(clientData);
+                }
+            }
+            #endregion
+
+            #region Create CSV
+            path = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\FilesTo"}";
+            using (var write = new StreamWriter(path + "\\NewFile.csv"))
+            using (var csv = new CsvWriter(write, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(clientsData);
+
+            }
+            #endregion
+
+            return clientsData;
+        }
+    }
+}
+   
