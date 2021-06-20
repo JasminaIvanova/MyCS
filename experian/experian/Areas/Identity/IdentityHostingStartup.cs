3b93715cddfe6ee7b8a9c842783a1342d6a255e6
Jasmina Ivanova <jasmina.j.ivanova@gmail.com>
2021-06-20 19:27:15 +0300

    ne znam

---
+++ experian/experian/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using experian.Areas.Identity.Data;
+using experian.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(experian.Areas.Identity.IdentityHostingStartup))]
+namespace experian.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<dbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("dbContextConnection")));
+
+                services.AddDefaultIdentity<appUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<dbContext>();
+            });
+        }
+    }
+}
---
+++ experian/experian/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using experian.Areas.Identity.Data;
+using experian.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace experian.Controllers
+{
+    [Authorize(Roles = "SuperAdmin")]
+    public class AdminController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+        public IActionResult CreateUser(IFormCollection form, IServiceProvider services = ) 
+        {
+
+            var UserManager = services.GetRequiredService<UserManager<appUser>>();
+            string username = form["txtEmail"];
+            string email = form["txtEmail"];
+            string passw = form["txtPassword"];
+
+            var newUser = new appUser();
+            newUser.UserName = username;
+            newUser.Email = email;
+            newUser.EmailConfirmed = true;
+
+            var admin = UserManager.CreateAsync(newUser, passw);
+            return View();
+        }
+
+    }
+}
